592d0a262725c113af697b4f292375be7f1d8cf4
dongkyunkimdev <ddkds66@gmail.com>
2023-01-07 19:54:58 +0900

    회원가입 Application Service 작성

---
+++ src/main/kotlin/com/example/payhere/user/application/SignupService.kt
@@ -0,0 +1,49 @@
+package com.example.payhere.user.application
+
+import com.example.payhere.user.domain.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class SignupService(
+    private val userPersistencePort: UserPersistencePort,
+    private val passwordEncoder: BCryptPasswordEncoder
+) {
+    @Transactional
+    fun command(command: SignupCommand): SignupInfo {
+        if (userPersistencePort.existsUserByUsername(command.username)) {
+            throw DuplicateUsernameException(command.username)
+        }
+        command.password = passwordEncoder.encode(command.password)
+        val savedUser = userPersistencePort.saveUser(command.toEntity())
+
+        return SignupInfo.from(savedUser)
+    }
+
+    data class SignupCommand(
+        val username: String,
+        var password: String,
+        val name: String
+    ) {
+        fun toEntity(): User = User(
+            username = this.username,
+            password = this.password,
+            name = this.name
+        )
+    }
+
+    data class SignupInfo(
+        val id: String,
+        val username: String,
+        val name: String
+    ) {
+        companion object {
+            fun from(user: User) = SignupInfo(
+                id = user.getId(),
+                username = user.username,
+                name = user.name
+            )
+        }
+    }
+}
