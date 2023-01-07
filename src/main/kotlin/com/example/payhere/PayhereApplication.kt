2391e85b899f0a9690fb284694a4cfd7e46c739b
dongkyunkimdev <ddkds66@gmail.com>
2023-01-07 16:00:16 +0900

    initial commit

---
+++ src/main/kotlin/com/example/payhere/PayhereApplication.kt
@@ -0,0 +1,11 @@
+package com.example.payhere
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayhereApplication
+
+fun main(args: Array<String>) {
+	runApplication<PayhereApplication>(*args)
+}
