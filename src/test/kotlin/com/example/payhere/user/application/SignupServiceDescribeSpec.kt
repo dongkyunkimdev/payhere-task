9dabb6cd904e760e6ca5df43817188b603427eeb
dongkyunkimdev <ddkds66@gmail.com>
2023-01-07 20:49:27 +0900

    성공하는 회원가입 단위 테스트 케이스 작성

--- src/test/kotlin/com/example/payhere/user/application/SignupServiceDescribeSpec.kt
+++ src/test/kotlin/com/example/payhere/user/application/SignupServiceDescribeSpec.kt
@@ -1,8 +1,10 @@
 package com.example.payhere.user.application
 
 import com.example.payhere.user.application.exception.DuplicateUsernameException
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -13,6 +15,19 @@ class SignupServiceDescribeSpec : DescribeSpec({
     val signupService = SignupService(mockUserPersistencePort, passwordEncoder)
 
     describe("signup") {
+        context("회원가입되지 않은 username을 가진 Command가 주어지면") {
+            every { mockUserPersistencePort.saveUser(any()) } answers { notDuplicatedUsernameCommand.toEntity() }
+            every { mockUserPersistencePort.existsUserByUsername(notDuplicatedUsernameCommand.username) } answers { false }
+            it("회원가입에 성공하고 command와 동일한 값을 가진 SignupInfo 응답") {
+                val resultInfo = signupService.command(notDuplicatedUsernameCommand)
+
+                assertSoftly {
+                    resultInfo.username shouldBe notDuplicatedUsernameCommand.username
+                    resultInfo.name shouldBe notDuplicatedUsernameCommand.name
+                }
+            }
+        }
+
         context("이미 회원가입된 username을 가진 Command가 주어지면") {
             every { mockUserPersistencePort.existsUserByUsername(duplicatedUsernameCommand.username) } answers { true }
             it("DuplicateUsernameException 발생") {
@@ -25,6 +40,12 @@ class SignupServiceDescribeSpec : DescribeSpec({
 }) {
     companion object {
         private val duplicatedUsernameCommand = SignupService.SignupCommand(
+            username = "username@email.com",
+            password = "password",
+            name = "name"
+        )
+
+        private val notDuplicatedUsernameCommand = SignupService.SignupCommand(
             username = "username@email.com",
             password = "password",
             name = "name"
