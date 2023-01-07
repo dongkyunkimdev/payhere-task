414a1c58f431369823e92b591f2b0734d7d087f7
dongkyunkimdev <ddkds66@gmail.com>
2023-01-07 21:14:36 +0900

    가계부 작성 application service 작성

---
+++ src/main/kotlin/com/example/payhere/ledger/application/WriteLedgerService.kt
@@ -0,0 +1,33 @@
+package com.example.payhere.ledger.application
+
+import com.example.payhere.ledger.domain.Ledger
+import com.example.payhere.user.application.UserPersistencePort
+import com.example.payhere.user.application.exception.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class WriteLedgerService(
+    private val ledgerPersistencePort: LedgerPersistencePort,
+    private val userPersistencePort: UserPersistencePort
+) {
+    @Transactional
+    fun command(command: WriteLedgerCommand): WriteLedgerInfo {
+        val savedUser = userPersistencePort.findUserByUsername(command.username)
+        savedUser ?: throw UsernameNotFoundException(command.username)
+        val savedLedger = ledgerPersistencePort.saveLedger(Ledger(command.price, command.memo, savedUser))
+
+        return WriteLedgerInfo(price = savedLedger.price, memo = savedLedger.memo)
+    }
+
+    data class WriteLedgerCommand(
+        val username: String,
+        val price: Long,
+        val memo: String
+    )
+
+    data class WriteLedgerInfo(
+        val price: Long,
+        val memo: String
+    )
+}
