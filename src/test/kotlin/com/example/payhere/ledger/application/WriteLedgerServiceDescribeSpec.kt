a07c003830db71cee152bc27c09cf17b7a1a2c50
dongkyunkimdev <ddkds66@gmail.com>
2023-01-08 17:46:45 +0900

    가계부 작성 application service의 성공하는 단위 테스트 케이스 작성

--- src/test/kotlin/com/example/payhere/ledger/application/WriteLedgerServiceDescribeSpec.kt
+++ src/test/kotlin/com/example/payhere/ledger/application/WriteLedgerServiceDescribeSpec.kt
@@ -1,9 +1,13 @@
 package com.example.payhere.ledger.application
 
+import com.example.payhere.ledger.domain.Ledger
 import com.example.payhere.user.application.UserPersistencePort
 import com.example.payhere.user.application.exception.UsernameNotFoundException
+import com.example.payhere.user.domain.User
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 
@@ -18,6 +22,21 @@ class WriteLedgerServiceDescribeSpec : DescribeSpec({
             it("UsernameNotFoundException 발생") {
                 shouldThrow<UsernameNotFoundException> {
                     writeLedgerService.command(notExistsUsernameCommand)
+                }
+            }
+        }
+
+        context("존재하는 username을 가진 Command가 주어지면") {
+            val expectedUser = User(existsUsernameCommand.username, "password")
+            val expectedLedger = Ledger(existsUsernameCommand.price, existsUsernameCommand.memo, expectedUser)
+            every { mockUserPersistencePort.findUserByUsername(existsUsernameCommand.username) } answers { expectedUser }
+            every { mockLedgerPersistencePort.saveLedger(any()) } answers { expectedLedger }
+            it("가계부 작성에 성공하고 올바른 값을 가진 WriteLedgerInfo 응답") {
+                val info = writeLedgerService.command(existsUsernameCommand)
+
+                assertSoftly {
+                    info.price shouldBe existsUsernameCommand.price
+                    info.memo shouldBe existsUsernameCommand.memo
                 }
             }
         }
