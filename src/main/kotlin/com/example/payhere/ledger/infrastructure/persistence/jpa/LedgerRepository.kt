b7b80d25c8bee6e0cca86731862a5d2958ddf1e5
dongkyunkimdev <ddkds66@gmail.com>
2023-01-08 18:22:25 +0900

    사용되지 않는 findLedgerById 쿼리 메서드 제거

--- src/main/kotlin/com/example/payhere/ledger/infrastructure/persistence/jpa/LedgerRepository.kt
+++ src/main/kotlin/com/example/payhere/ledger/infrastructure/persistence/jpa/LedgerRepository.kt
@@ -4,5 +4,4 @@ import com.example.payhere.ledger.domain.Ledger
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface LedgerRepository : JpaRepository<Ledger, String> {
-    fun findLedgerById(id: String): Ledger?
 }
