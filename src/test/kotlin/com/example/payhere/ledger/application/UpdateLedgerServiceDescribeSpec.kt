8a018f592ebe26568977d7c0794828e17e4feaca
dongkyunkimdev <ddkds66@gmail.com>
2023-01-08 19:09:23 +0900

    가계부 수정 application service의 성공하는 단위 테스트 케이스 작성

--- src/test/kotlin/com/example/payhere/ledger/application/UpdateLedgerServiceDescribeSpec.kt
+++ src/test/kotlin/com/example/payhere/ledger/application/UpdateLedgerServiceDescribeSpec.kt
@@ -1,8 +1,12 @@
 package com.example.payhere.ledger.application
 
 import com.example.payhere.ledger.application.exception.LedgerIdNotFoundException
+import com.example.payhere.ledger.domain.Ledger
+import com.example.payhere.user.domain.User
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 
@@ -16,6 +20,19 @@ class UpdateLedgerServiceDescribeSpec : DescribeSpec({
             it("LedgerIdNotFoundException 발생") {
                 shouldThrow<LedgerIdNotFoundException> {
                     updateLedgerService.command(notExistsLedgerIdCommand)
+                }
+            }
+        }
+
+        context("존재하는 ledgerId를 가진 Command가 주어지면") {
+            val expectedLedger = Ledger(price = 10000, memo = "memo", user = User("username", "password"))
+            every { mockLedgerPersistencePort.findLedgerById(existsLedgerIdCommand.ledgerId) } answers { expectedLedger }
+            it("가계부 수정에 성공하고 command와 일치하는 값을 가진 UpdateLedgerInfo 응답") {
+                val info = updateLedgerService.command(existsLedgerIdCommand)
+
+                assertSoftly {
+                    info.price shouldBe existsLedgerIdCommand.price
+                    info.memo shouldBe existsLedgerIdCommand.memo
                 }
             }
         }
