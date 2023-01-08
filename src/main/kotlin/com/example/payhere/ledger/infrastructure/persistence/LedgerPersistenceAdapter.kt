2a80945f77906c7529fe7d4bf70c9ddb33dd82fb
dongkyunkimdev <ddkds66@gmail.com>
2023-01-08 18:22:03 +0900

    가계부와 유저의 관계에서 발생하는 n+1 쿼리 해결을 위해 querydsl을 사용하여 fetch join

--- src/main/kotlin/com/example/payhere/ledger/infrastructure/persistence/LedgerPersistenceAdapter.kt
+++ src/main/kotlin/com/example/payhere/ledger/infrastructure/persistence/LedgerPersistenceAdapter.kt
@@ -3,12 +3,14 @@ package com.example.payhere.ledger.infrastructure.persistence
 import com.example.payhere.ledger.application.LedgerPersistencePort
 import com.example.payhere.ledger.domain.Ledger
 import com.example.payhere.ledger.infrastructure.persistence.jpa.LedgerRepository
+import com.example.payhere.ledger.infrastructure.persistence.quqerydsl.LedgerRepositorySupport
 import org.springframework.stereotype.Component
 
 @Component
 class LedgerPersistenceAdapter(
-    private val ledgerRepository: LedgerRepository
+    private val ledgerRepository: LedgerRepository,
+    private val ledgerRepositorySupport: LedgerRepositorySupport
 ) : LedgerPersistencePort {
     override fun saveLedger(ledger: Ledger): Ledger = ledgerRepository.save(ledger)
-    override fun findLedgerById(id: String): Ledger? = ledgerRepository.findLedgerById(id)
+    override fun findLedgerById(id: String): Ledger? = ledgerRepositorySupport.findLedgerById(id)
 }
---
+++ src/main/kotlin/com/example/payhere/ledger/infrastructure/persistence/quqerydsl/LedgerRepositorySupport.kt
@@ -0,0 +1,19 @@
+package com.example.payhere.ledger.infrastructure.persistence.quqerydsl
+
+import com.example.payhere.ledger.domain.Ledger
+import com.example.payhere.ledger.domain.QLedger
+import com.example.payhere.user.domain.QUser
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.stereotype.Repository
+
+@Repository
+class LedgerRepositorySupport(
+    private val queryFactory: JPAQueryFactory
+) {
+    fun findLedgerById(id: String): Ledger? =
+        queryFactory.selectFrom(QLedger.ledger)
+            .where(QLedger.ledger.id.eq(id))
+            .leftJoin(QLedger.ledger.user, QUser.user)
+            .fetchJoin()
+            .fetchOne()
+}
